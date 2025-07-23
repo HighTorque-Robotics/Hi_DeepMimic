4e3b74d600acf17a2ed0d408fb1ba1e14e6cfe79
zixiangme <2742974891@qq.com>
2025-07-23 12:51:44 +0800

    pushup training baseline

---
+++ mimic_hi-main/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='mimic_real',
+    packages=find_packages(),
+    version="0.0.1",
+    install_requires=[
+        ]
+)
